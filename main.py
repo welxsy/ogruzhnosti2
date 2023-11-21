1f03a3d7c3344e66c86f84bedce4a0226b55c428
welxsy <vika080308@gmail.com>
2023-11-21 20:36:59 +0300

    Fifth commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QVBoxLayout, QGraphicsScene, QGraphicsView
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+import random
 
 
 class CircleInterface(QWidget):
@@ -9,10 +12,19 @@ class CircleInterface(QWidget):
         self.button = QPushButton('Создать', self)
         self.button.clicked.connect(self.create_random_circle)
 
+        self.scene = QGraphicsScene(self)
+        self.view = QGraphicsView(self.scene)
+        self.view.setAlignment(Qt.AlignTop)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.view)
+        layout.addWidget(self.button)
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
