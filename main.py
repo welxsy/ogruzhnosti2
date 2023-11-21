e941ad7da58f070a3adcadd8b8aa3614822fde06
welxsy <vika080308@gmail.com>
2023-11-21 20:35:12 +0300

    Fourth commit

--- main.py
+++ main.py
@@ -14,4 +14,10 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
 
