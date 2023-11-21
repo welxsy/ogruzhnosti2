457167b224a6b68aacc8a5d5edd9e7cf5e6999cf
welxsy <vika080308@gmail.com>
2023-11-21 20:32:20 +0300

    First commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+
+
+class CircleInterface(QWidget):
+    def __init__(self):
+        super(CircleInterface, self).__init__()
+
